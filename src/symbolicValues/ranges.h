197445d981d7cf72f741728b7cdeb6891b2be69b
Matas <m.aizenas@student.utwente.nl>
2024-07-24 21:16:31 +0200

    implemented function to search ranges for the one containing a certain value

--- src/symbolicValues/ranges.h
+++ src/symbolicValues/ranges.h
@@ -11,12 +11,14 @@ struct range
     unsigned long long LTOE; 
     unsigned long long MTOE;
     struct range *next_range; 
+    int function_success; //used for functions to say if they were successful in completing a certain operation on the range. 1 = successful, 0 otherwise
 };
 
 //makes a new range
 range makeRange(unsigned long long, unsigned long long new_MTOE, range *new_next_range);
 //displays a range in a nice way. Takes care of split ranges as well
 void displayRange(struct range range_to_display);
-
+//locates a part of the range containing the given num to search for. Returns a range with success 0 if it failed
+range locate_part_containing(range, unsigned long long);
 
 #endif // __MYCLASS_H_INCLUDED__ 
